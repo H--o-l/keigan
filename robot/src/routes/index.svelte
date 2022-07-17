f1a74ae7cdd3bd4124a8d745abd681fd5b639922
Hoel IRIS <hoel.iris@gmail.com>
2022-07-17 11:07:54 +0200

    svelte init

---
+++ robot/src/routes/index.svelte
@@ -0,0 +1,16 @@
+
+<script lang='ts'>
+  import { browser } from '$app/env';
+  import { onMount } from 'svelte';
+  import './../KMConnectorBrowser.js?client'
+
+  if (browser) {  // client-only code here
+    const toto = new (window as any).KMMotorOneWebBLE();
+    console.warn(toto)
+  }
+
+  let count: number = 0;
+</script>
+
+<input type=number bind:value={count}>
+<button on:click="{() => count += 1}">count</button>
