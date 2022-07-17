f5a0b6b106b8514b8728397783090d0abbe1b8b6
Hoel IRIS <hoel.iris@gmail.com>
2022-07-17 12:36:46 +0200

    import of KMConnectorBrowser.js client side

--- robot/src/routes/index.svelte
+++ robot/src/routes/index.svelte
@@ -1,15 +1,14 @@
 
 <script lang='ts'>
-  import { browser } from '$app/env';
-  import { onMount } from 'svelte';
-  import './../KMConnectorBrowser.js?client'
+  import {browser} from '$app/env';
+  import './../KMConnectorBrowser.js?client';
 
   if (browser) {  // client-only code here
     const toto = new (window as any).KMMotorOneWebBLE();
     console.warn(toto)
   }
 
-  let count: number = 0;
+  let count = 0;
 </script>
 
 <input type=number bind:value={count}>
